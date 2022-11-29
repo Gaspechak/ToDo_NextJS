40f9a3ecb26a5d8137c74022ef8935c1dfa37eb7
Gaspechak <mathgaspechak@outlook.com>
2022-11-29 14:53:45 -0400

    Implementação inicial bootstrap

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useSWRConfig } from 'swr'
+import { InputGroup, Form, Button } from 'react-bootstrap';
 
 const postNewTodo = function (todo, mutate) {
     mutate('/api/todos', async todos => {
@@ -27,32 +28,25 @@ export default function TodoForm(props) {
     const { mutate } = useSWRConfig()
 
     const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
 
     return (
-        <div className='todo-form input-group'>
-            <input className='title-input'
-                type="text"
-                placeholder="O que você precisa fazer?"
-                value={title}
-                onChange={e => setTitle(e.target.value)}
-                onKeyUp={(e) => checkKeyPressed(e, () => {
-                    postNewTodo({ title, description }, mutate)
-                    setTitle("")
-                })} />
-
-
-            <button className='add-button' onClick={() => {
-                postNewTodo({ title, description }, mutate)
-                setTitle("")
-            }}>
-
-                <svg width="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path fillRule="evenodd" d="M12 3.75a.75.75 0 01.75.75v6.75h6.75a.75.75 0 010 1.5h-6.75v6.75a.75.75 0 01-1.5 0v-6.75H4.5a.75.75 0 010-1.5h6.75V4.5a.75.75 0 01.75-.75z" clipRule="evenodd" />
-                </svg>
-
-            </button>
-
-        </div>
+        <>
+            <div className='mt-4'>
+                <InputGroup className="mb-3">
+                    <Form.Control
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        onKeyUp={(e) => checkKeyPressed(e, () => {
+                            postNewTodo({ title, description: '' }, mutate)
+                            setTitle("")
+                        })}
+                        placeholder="O que você precisa fazer?" />
+                    <Button variant="success" onClick={() => {
+                        postNewTodo({ title, description: '' }, mutate)
+                        setTitle("")
+                    }}>Adicionar</Button>
+                </InputGroup>
+            </div>
+        </>
     )
 }
